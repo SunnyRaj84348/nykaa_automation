d011ae0f6d47693137b57d2992a51f1ece9907ce
SunnyRaj84348 <sunnyraj7461@gmail.com>
2025-04-20 12:04:03 +0530

    Revert "webhook test"
    This reverts commit e24b080ac8a9ac4636dcd94fb0989f10bf5a0630.

--- src/test/java/nykaatest/ExampleTest.java
+++ src/test/java/nykaatest/ExampleTest.java
@@ -23,6 +23,6 @@ public class ExampleTest extends Base {
 
     @Test(priority = 3)
     public void test4() {
-        Assert.assertTrue("A".equals("B"));
+        Assert.assertTrue("A".equals("A"));
     }
 }
